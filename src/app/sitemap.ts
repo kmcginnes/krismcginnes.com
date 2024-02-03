3f58e89a85c0b48b96f6c6885bb10ce890cc48f7
Kris McGinnes <kmcginnes@me.com>
2024-02-02 22:21:13 -0600

    Add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next"
+import { env } from "~/env"
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: env.SITE_URL ?? "http://localhost:3000",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ]
+}
