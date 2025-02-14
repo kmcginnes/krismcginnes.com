4b836a4ff0155c683ca7adcefaa5eeeacbf72197
Kris McGinnes <kmcginnes@me.com>
2025-02-14 09:57:25 -0600

    Update to Next 15

--- next.config.js
+++ next.config.js
@@ -6,13 +6,8 @@ await import("./src/env.js")
 
 /** @type {import("next").NextConfig} */
 const config = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  swcMinify: true,
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 }
 
 export default config
