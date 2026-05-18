a22e563077e7ee701d9ed03cae3c0a6150e7717b
Kris McGinnes <kmcginnes@me.com>
2026-05-18 10:46:40 -0700

    Update to latest dependencies (#14)
    * Update to latest dependencies
    
    * Fix TS error

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ await import("./src/env.js")
 /** @type {import("next").NextConfig} */
 const config = {
   typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
 }
 
 export default config
